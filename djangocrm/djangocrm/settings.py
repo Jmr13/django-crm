6c51f2667635785810043067d559f5b77500c298
Jmr13 <Gamayanjomar@yahoo.com>
2024-12-15 12:32:48 +0800

    Added login, logout, register functionality

--- djangocrm/djangocrm/settings.py
+++ djangocrm/djangocrm/settings.py
@@ -25,6 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Add the name of the project
+    'website'
 ]
 
 MIDDLEWARE = [
